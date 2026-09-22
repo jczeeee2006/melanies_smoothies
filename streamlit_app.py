d6571af873192a9abda0d427a96ea5296dcf5a58
jczeeee2006 <96461380+jczeeee2006@users.noreply.github.com>
2026-09-22 18:04:14 +0800

    Customize smoothie orders with ingredient selection
    Updated the smoothie order app to allow customization of smoothies by selecting ingredients and entering a name. Changed the title and adjusted the data handling for orders.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,53 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col, when_matched
+from snowflake.snowpark.functions import col
+
 
 # Write directly to the app
-st.title(":cup_with_straw: Pending Smoothie Orders! :cup_with_straw:")
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 
 st.write(
     """
-    **Orders that need to be filled.**
+    **Choose the fruits you want in your custom Smoothie!**
     """
 )
 
 
-# Get Snowflake session
+# Add a name for the smoothie
+name_on_order = st.text_input("Name on Smoothie:")
+
+st.write("The name on your Smoothie will be:", name_on_order)
+
+
 session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+#st.dataframe(data=my_dataframe, use_container_width=True)
 
-my_dataframe = session.table("smoothies.public.orders") \
-    .filter(col("ORDER_FILLED") == 0)
+ingredients_list = st.multiselect(
+    'Choose up yo 5 ingridients:'
+    , my_dataframe
+    , max_selections=5
+    )
 
-editable_df = st.data_editor(my_dataframe)
+if ingredients_list:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+    #st.write(ingredients_string)
 
 
-submitted = st.button("Submit")
+    my_insert_stmt = f"""
+    insert into smoothies.public.orders(ingredients, name_on_order)
+    values ('{ingredients_string}', '{name_on_order}')"""
 
-if submitted:
-    og_dataset = session.table("smoothies.public.orders")
-    edited_dataset = session.create_dataframe(editable_df)
 
-    try:
-        og_dataset.merge(
-            edited_dataset,
-            (og_dataset["ORDER_UID"] == edited_dataset["ORDER_UID"]),
-            [when_matched().update({"ORDER_FILLED": edited_dataset["ORDER_FILLED"]})]
-        )
+    #st.write(my_insert_stmt)
 
-        st.success("Order(s) Updated!", icon="👍")
+    time_to_insert = st.button('Submit Order')
 
-    except:
-        st.write("Something went wrong.")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon='✅')
